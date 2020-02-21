322aef937ee500dbde448500ce69c49a54987656
sebastianbenitez <58483040+sebastianbenitez@users.noreply.github.com>
2020-02-21 01:12:49 -0300

    Ajustadas nomenclaturas, separacion en funciones
    * Eliminados rastros de spanglish
    * separacion de responsabilidades en funciones para facilitar lectura y mantenimiento

--- PrettyVersion/index.js
+++ PrettyVersion/index.js
@@ -2,27 +2,55 @@
 var random1 = Math.floor(Math.random()*100);
 var random2 = Math.floor(Math.random()*100);
 
-document.addEventListener('DOM', function() {
-	alert('click');
-},false);
+document.addEventListener('DOMContentLoaded', function() {
+	
+	var h2Number1 = document.getElementById('h2--number-1');
+	h2Number1.innerHTML = random1;
 
-document.getElementById('Number1').innerHTML = random1;
+	var spanResponse = document.getElementById('span--response');
+	var divNumber2Container = document.getElementById('div--number-2-container');
+	//var divGameButtonsContainer = document.getElementById('div--game-buttons-container');
+	
+	document.getElementById('btn--higher').addEventListener('click', function() {
+		playGame('higher', spanResponse, divNumber2Container);
+	});
+	document.getElementById('btn--lower').addEventListener('click', function() {
+		playGame('lower', spanResponse, divNumber2Container);
+	});
+},false);
 
 
 function validateHigherLower(guess) {
-	var guessed = (guess == 'higher' && random2 > random1) || (guess == 'lower' && random2 < random1) ?true :false;
-	var response = document.getElementById('response');
-	response.classList.remove('fa-angry');
-	response.classList.remove('m-fadeOut');
-	response.classList.remove('m-fadeIn');
-	//response.style.visibility = 'visible';
-	
-	response.classList.add('m-fadeIn');
+	return (guess == 'higher' && random2 > random1) || (guess == 'lower' && random2 < random1);
+}
+
+function setSuccessResponse(spanResponse) {
+	spanResponse.classList.remove('fa-times-circle','text-danger');
+	spanResponse.classList.add('fa-check-circle','text-success');
+}
+
+function setErrorResponse(spanResponse) {
+	spanResponse.classList.remove('fa-check-circle','text-success');
+	spanResponse.classList.add('fa-times-circle','text-danger');
+}
+
+function showResponse(spanResponse) {
+	spanResponse.classList.remove('fa-angry');
+	spanResponse.classList.remove('smoothly-hide');
+	spanResponse.classList.add('smoothly-visible');
+}
+
+function newGame(spanResponse) {
+	spanResponse.classList.remove('smoothly-visible');
+	spanResponse.classList.add('smoothly-hide');
+}
+
+function playGame(guess, spanResponse, divNumber2Container) {
+	var guessed = validateHigherLower(guess);
 	if (guessed) {
-		response.classList.remove('fa-times-circle','text-danger');
-		response.classList.add('fa-check-circle','text-success');
-		return;
+		setSuccessResponse(spanResponse);
+	} else {
+		setErrorResponse(spanResponse);
 	}
-	response.classList.remove('fa-check-circle','text-success');
-	response.classList.add('fa-times-circle','text-danger');
+	showResponse(spanResponse);
 }
