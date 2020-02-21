51b27428fbdca27b9fa6ee25f17cfd516e88ffd4
sebastianbenitez <58483040+sebastianbenitez@users.noreply.github.com>
2020-02-21 19:39:28 -0300

    Agregado Version fea

---
+++ UglyVersion/index.js
@@ -0,0 +1,24 @@
+
+function positionTop() {
+	return Math.floor(Math.random() * 20);
+}
+
+function marginLeft() {
+	return Math.floor(Math.random() * 70);
+}
+
+document.addEventListener('DOMContentLoaded',function () {
+	var divGameContainer = document.getElementById('div--game-container');
+	divGameContainer.innerHTML = '<object type="text/html" data="../PrettyVersion/index.html" width="100%" height="550px" ></object>';
+
+	var adsModals = $('.modal-ad');
+
+	setInterval(function () {
+		for (var i = 0; i < adsModals.length; i++) {
+			$(adsModals[i]).find('.modal-content').css('top', positionTop()+'vh');
+			$(adsModals[i]).find('.modal-dialog').css('margin-left', marginLeft()+'%');
+			$(adsModals[i]).modal('show');
+		}
+	}, 6000);
+	
+},false);
